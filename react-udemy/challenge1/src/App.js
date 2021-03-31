74a917ad74c7b0284c1e3bc116741f85d86e4114
roy yoo <royyoo123@gmail.com>
2021-03-31 00:18:37 -0700

    tests

--- react-udemy/challenge1/src/App.js
+++ react-udemy/challenge1/src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
+import UserInput from './components/UserInput';
+import UserOutput from './components/UserOutput';
+import React, { Component } from 'react';
+class App extends Component {
+	state = {
+		username: 'supermax'
+	}
+	render() {
+		return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserInput />
+      <UserOutput userName={this.state.username}/>
+      <UserOutput userName="Billy"/>
     </div>
-  );
+  	);
+	}
 }
 
 export default App;
---
+++ react-udemy/challenge1/src/components/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserOutput = (props) => {
+	return (
+		<div className="UserOutput">
+			<p>Username: {props.userName}</p>
+			<p>hi</p>
+		</div>
+	)
+};
+
+export default UserOutput;
